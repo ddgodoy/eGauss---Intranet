67e1a08611a313982ff253360bf89ccf7ec9db26
Mauro <mauro@samarcoweb.com>
2014-01-21 09:08:12 -0300

    hotfixt

--- apps/intranet/modules/home/actions/actions.class.php
+++ apps/intranet/modules/home/actions/actions.class.php
@@ -24,7 +24,7 @@ class homeActions extends sfActions
             $client = new Google_Client();
             $client->setClientId('394341489547.apps.googleusercontent.com');
             $client->setClientSecret('394341489547@developer.gserviceaccount.com');
-            $client->setRedirectUri('http://egauss-intranet.icox.com/');
+            $client->setRedirectUri('http://egauss-intranet.icox.com/intranet_dev.php');
             $client->setScopes(array('https://www.googleapis.com/auth/drive'));
             $client->setAccessType('online'); 
 
