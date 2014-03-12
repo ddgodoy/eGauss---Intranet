36527b1437015fc8a16948943c15a5bf667f1c6d
mauro@icox.com.ar <mauro@icox.com.ar>
2014-03-12 16:09:01 -0300

    hotfix - documentos

--- apps/intranet/modules/analyzed/templates/_getDocumentView.php
+++ apps/intranet/modules/analyzed/templates/_getDocumentView.php
@@ -20,18 +20,18 @@ $(document).ready(function()
     <table width="100%" cellspacing="4" cellpadding="0" border="0">
         <?php foreach($result_document AS $k=>$v):?>
         <tr>
-            <td width="3%" class="text_detail"><a class="fancybox-manual-d" dir="<?php echo $v['id'] ?>" style="text-decoration: none; cursor: pointer "><?php echo Common::getFormattedDate($v['date'] , 'd/m/Y') ?></a></td>
+            <td width="10%" class="text_detail"><a class="fancybox-manual-d" dir="<?php echo $v['id'] ?>" style="text-decoration: none; cursor: pointer "><?php echo Common::getFormattedDate($v['date'] , 'd/m/Y') ?></a></td>
             <td width="35%" class="text_detail">
                 <a class="fancybox-manual-d" dir="<?php echo $v['id'] ?>" style="text-decoration: none; cursor: pointer ">
                     <label style="cursor: pointer"><strong><?php echo $v['name'] ?></strong></label>
                 </a>        
             </td>
-            <td>
+            <td width="10%">
                 <a  href="<?php echo $v['url'] ?>" target="_blanck">
                     <img src="<?php echo $v['icon'] ?>" border="0" style="width:20px;height:20px;" title="Ver"/>
                 </a>
             </td>
-            <td>
+            <td width="10%">
                 <a  href="<?php echo $v['download']!=''?$v['download']:$v['url'] ?>" <?php if($v['download']==''): ?> target="_blanck" <?php endif; ?>>
                     <img src="/images/descargar-documento.jpg" border="0" style="width:20px;height:20px;" title="Descargar"/>
                 </a>    
