218f6699bf9ec822d8e9ddcfd58fb0915a2bbaf7
mauro@icox.com.ar <mauro@icox.com.ar>
2014-03-17 14:29:10 -0300

    hotfix - Inversores

--- apps/intranet/modules/home/actions/actions.class.php
+++ apps/intranet/modules/home/actions/actions.class.php
@@ -142,15 +142,15 @@ class homeActions extends sfActions
     */
    protected  function get_oauth2_token($grantCode) {
         
-        $oauth2token_url = "https://accounts.google.com/o/oauth2/auth";
+        $oauth2token_url = "https://accounts.google.com/o/oauth2/token";
         $clienttoken_post = array(
         "client_id" => '394341489547.apps.googleusercontent.com',
         "client_secret" => 'EqhEQdb4YDZc4ZxXtIh1HskA');
 
         
-        $clienttoken_post["refresh_token"] = $grantCode;
-        $clienttoken_post["grant_type"] = "refresh_token";
-        
+        $clienttoken_post["code"] = $grantCode;
+        $clienttoken_post["redirect_uri"] = 'http://egauss-intranet.icox.com';
+        $clienttoken_post["grant_type"] = "authorization_code";
         
         $curl = curl_init($oauth2token_url);
 
